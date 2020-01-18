36970d35edd7f672bad44fd9d2d69e734e1cd9a1
mrdulin <novaline.dulin@gmail.com>
2020-01-18 17:51:17 +0800

    improve sample

--- src/examples/Getters-setters/index.test.ts
+++ src/examples/Getters-setters/index.test.ts
@@ -7,8 +7,21 @@ describe('Getters&setters', () => {
   afterAll(async () => {
     await sequelize.close();
   });
-  it('should get full name', async () => {
+
+  it('should get tablename', () => {
+    const tableName = Foo.getTableName();
+    expect(tableName).toBe('foos');
+  });
+
+  it('should get/set full name and presist to database', async () => {
     const user = await Foo.create({ firstname: 'Lin', lastname: 'Du' });
     expect(user.fullName).toBe('Lin Du');
+    user.fullName = 'Bill Gates';
+    await user.save();
+    expect(user.firstname).toBe('Bill');
+    expect(user.lastname).toBe('Gates');
+    const presistedUser = await Foo.findOne({ where: { firstname: 'Bill' } });
+    expect(presistedUser.firstname).toBe('Bill');
+    expect(presistedUser.lastname).toBe('Gates');
   });
 });
