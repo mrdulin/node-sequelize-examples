ca82d34bb55e2d2326210bde6985a8c510cf1aca
mrdulin <novaline.dulin@gmail.com>
2020-01-22 16:56:30 +0800

    add sample

--- src/examples/associations/belongs-to-many-associations/index.test.ts
+++ src/examples/associations/belongs-to-many-associations/index.test.ts
@@ -2,20 +2,22 @@ import { sequelize, Project, User } from './';
 import faker from 'faker';
 
 describe('associations', () => {
+  const usersOfProject1 = [{ name: faker.name.findName() }, { name: faker.name.findName() }];
+  const usersOfProject2 = [
+    { name: faker.name.findName() },
+    { name: faker.name.findName() },
+    { name: faker.name.findName() },
+  ];
+
   beforeAll(async () => {
     await sequelize.sync({ force: true });
   });
   afterAll(async () => {
     await sequelize.close();
   });
+
   describe('belongs to many associations', () => {
     it('should create project and user data rows and UserProject table', async () => {
-      const usersOfProject1 = [{ name: faker.name.findName() }, { name: faker.name.findName() }];
-      const usersOfProject2 = [
-        { name: faker.name.findName() },
-        { name: faker.name.findName() },
-        { name: faker.name.findName() },
-      ];
       const projects: Project[] = await Project.bulkCreate(
         [
           { name: faker.commerce.productName(), users: usersOfProject1 },
@@ -27,6 +29,13 @@ describe('associations', () => {
       const userModelsOfProject1: User[] = await project1.getUsers();
       const userNamesOfProject1: Array<{ name: string }> = userModelsOfProject1.map((m: User) => ({ name: m.name }));
       expect(userNamesOfProject1).toEqual(usersOfProject1);
+    });
+
+    it('should get users by users property of Project model', async () => {
+      let project: Project = await Project.findByPk(1);
+      expect(project.users).toBeUndefined();
+      project = await Project.findByPk(1, { include: [Project.associations.users] });
+      expect(project.users).toHaveLength(usersOfProject1.length);
     });
   });
 });
--- src/examples/associations/belongs-to-many-associations/index.ts
+++ src/examples/associations/belongs-to-many-associations/index.ts
@@ -1,10 +1,22 @@
-import { Model, DataTypes, BelongsToManyGetAssociationsMixin } from 'sequelize';
+import {
+  Model,
+  DataTypes,
+  BelongsToManyGetAssociationsMixin,
+  BelongsToManyAddAssociationMixin,
+  Association,
+} from 'sequelize';
 import { sequelize } from '../../db';
 
 class Project extends Model {
   public id!: number;
   public name!: string;
   public getUsers!: BelongsToManyGetAssociationsMixin<User>;
+  public addUser!: BelongsToManyAddAssociationMixin<User, number>;
+  public readonly users?: User[];
+
+  public static associations: {
+    users: Association<Project, User>;
+  };
 }
 Project.init({ name: DataTypes.STRING }, { sequelize, modelName: 'project' });
 
@@ -12,6 +24,7 @@ class User extends Model {
   public id!: number;
   public name!: string;
   public getProjects!: BelongsToManyGetAssociationsMixin<Project>;
+  public addProject!: BelongsToManyAddAssociationMixin<Project, number>;
 }
 User.init({ name: DataTypes.STRING }, { sequelize, modelName: 'user' });
 
