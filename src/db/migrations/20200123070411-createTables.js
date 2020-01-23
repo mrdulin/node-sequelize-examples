56951280ad6bedc0faf8ce4d43058715ddad1e9c
mrdulin <novaline.dulin@gmail.com>
2020-01-23 17:32:48 +0800

    add migration and seed files

---
+++ src/db/migrations/20200123070411-createTables.js
@@ -0,0 +1,141 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Category', {
+      ID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Name: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      Last_Update: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable('Language', {
+      ID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Name: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      Last_Update: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable('Film', {
+      ID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      LanguageID: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Language',
+          },
+          key: 'ID',
+        },
+        allowNull: false,
+      },
+      Title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Release_Year: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Rental_Duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Rental_Date: {
+        type: Sequelize.DECIMAL(19, 0),
+        allowNull: false,
+      },
+      Length: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Replacement_Cost: {
+        type: Sequelize.DECIMAL(19, 0),
+        allowNull: false,
+      },
+      Rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Last_Update: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      Special_Features: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Fulltext: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+    await queryInterface.createTable(
+      'Film_Category',
+      {
+        FilmID: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: {
+              tableName: 'Film',
+            },
+            key: 'ID',
+          },
+        },
+        CategoryID: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: {
+              tableName: 'Category',
+            },
+            key: 'ID',
+          },
+        },
+        Last_Update: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      },
+      {
+        uniqueKeys: {
+          film_category_unique: {
+            fields: ['FilmID', 'CategoryID'],
+          },
+        },
+      },
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // return Promise.all(
+    //   ['Category', 'Language', 'Film_Category', 'Film'].map((tableName) => queryInterface.dropTable(tableName)),
+    // );
+    return queryInterface.dropAllTables();
+  },
+};
---
+++ src/db/seeders/20200123073339-createTables.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    try {
+      const transaction = await queryInterface.sequelize.transaction();
+      const bulkInsertLanguagesTask = queryInterface.bulkInsert('Language', [
+        { ID: 1, Name: faker.random.locale(), Last_Update: Sequelize.fn('NOW') },
+        { ID: 2, Name: faker.random.locale(), Last_Update: Sequelize.fn('NOW') },
+      ]);
+      const bulkInsertCategoriesTask = queryInterface.bulkInsert('Category', [
+        { ID: 1, Name: faker.lorem.word(), Last_Update: Sequelize.fn('NOW') },
+        { ID: 2, Name: faker.lorem.word(), Last_Update: Sequelize.fn('NOW') },
+      ]);
+
+      await Promise.all([bulkInsertLanguagesTask, bulkInsertCategoriesTask]);
+
+      let [languages, categories] = await Promise.all([
+        queryInterface.sequelize.query(`select * from "Language";`),
+        queryInterface.sequelize.query(`select * from "Category";`),
+      ]);
+      languages = languages[0];
+      categories = categories[0];
+
+      await queryInterface.bulkInsert('Film', [
+        {
+          ID: 1,
+          LanguageID: languages[0].ID,
+          Title: faker.lorem.sentence(),
+          Description: faker.lorem.sentence(),
+          Release_Year: faker.random.number({ min: 1900, max: 2030 }),
+          Rental_Duration: faker.random.number({ min: 0, max: 1000 }),
+          Rental_Date: faker.random.number({ min: 0, precision: 0.01 }),
+          Length: faker.random.number({ min: 0, max: 100 }),
+          Replacement_Cost: faker.random.number({ min: 0, precision: 0.01 }),
+          Rating: faker.random.number({ min: 1, max: 10 }),
+          Last_Update: Sequelize.fn('NOW'),
+          Special_Features: faker.lorem.word(),
+          Fulltext: faker.lorem.sentence(),
+        },
+        {
+          ID: 2,
+          LanguageID: languages[0].ID,
+          Title: faker.lorem.sentence(),
+          Description: faker.lorem.sentence(),
+          Release_Year: faker.random.number({ min: 1900, max: 2030 }),
+          Rental_Duration: faker.random.number({ min: 0, max: 1000 }),
+          Rental_Date: faker.random.number({ min: 0, precision: 0.01 }),
+          Length: faker.random.number({ min: 0, max: 100 }),
+          Replacement_Cost: faker.random.number({ min: 0, precision: 0.01 }),
+          Rating: faker.random.number({ min: 1, max: 10 }),
+          Last_Update: Sequelize.fn('NOW'),
+          Special_Features: faker.lorem.word(),
+          Fulltext: faker.lorem.sentence(),
+        },
+        {
+          ID: 3,
+          LanguageID: languages[1].ID,
+          Title: faker.lorem.sentence(),
+          Description: faker.lorem.sentence(),
+          Release_Year: faker.random.number({ min: 1900, max: 2030 }),
+          Rental_Duration: faker.random.number({ min: 0, max: 1000 }),
+          Rental_Date: faker.random.number({ min: 0, precision: 0.01 }),
+          Length: faker.random.number({ min: 0, max: 100 }),
+          Replacement_Cost: faker.random.number({ min: 0, precision: 0.01 }),
+          Rating: faker.random.number({ min: 1, max: 10 }),
+          Last_Update: Sequelize.fn('NOW'),
+          Special_Features: faker.lorem.word(),
+          Fulltext: faker.lorem.sentence(),
+        },
+      ]);
+
+      let films = await queryInterface.sequelize.query(`select * from "Film";`);
+      films = films[0];
+
+      await queryInterface.bulkInsert('Film_Category', [
+        { FilmID: films[0].ID, CategoryID: categories[0].ID, Last_Update: Sequelize.fn('NOW') },
+        { FilmID: films[0].ID, CategoryID: categories[1].ID, Last_Update: Sequelize.fn('NOW') },
+        { FilmID: films[1].ID, CategoryID: categories[1].ID, Last_Update: Sequelize.fn('NOW') },
+      ]);
+      await transaction.commit();
+    } catch (error) {
+      console.error(error);
+      await transaction.rollback();
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await Promise.all([
+      ['Language', 'Category', 'Film_Category', 'Film'].map((tableName) =>
+        queryInterface.bulkDelete(tableName, null, {}),
+      ),
+    ]);
+  },
+};
