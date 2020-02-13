3143567baa3eddc6cf78cfa673117682a08a2641
mrdulin <novaline.dulin@gmail.com>
2020-02-13 17:50:11 +0800

    add sample

---
+++ src/examples/stackoverflow/59877140/index.ts
@@ -0,0 +1,50 @@
+import { sequelize } from '../../db';
+import { Model, DataTypes } from 'sequelize';
+import assert from 'assert';
+
+interface SomeEntityModel extends Model {
+  result: any;
+  result_success: boolean;
+}
+
+class SomeEntity extends Model implements SomeEntityModel {
+  public id!: number;
+  public result!: any;
+  public result_success!: boolean;
+}
+SomeEntity.init(
+  {
+    result: {
+      type: DataTypes.JSONB,
+      defaultValue: {},
+    },
+    result_success: {
+      type: DataTypes.VIRTUAL,
+      get(this: SomeEntityModel) {
+        return this.getDataValue('result').success;
+      },
+    },
+  },
+  { sequelize, modelName: 'SomeEntities' },
+);
+
+(async function test() {
+  try {
+    await sequelize.sync({ force: true });
+    // seed
+    const entity = await SomeEntity.create({
+      result: {
+        score: { scaled: 1, raw: 8, min: 7, max: 10 },
+        success: true,
+        completion: true,
+        duration: '2H30M',
+      },
+    });
+    assert(entity.result_success, 'The value of the virtual field result_success should be truthy');
+    console.log('entity.result_success: ', entity.result_success);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await sequelize.close();
+  }
+})();
