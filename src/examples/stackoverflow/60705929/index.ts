a5968c19a0398b4b336196a3720eb20a4b62d657
mrdulin <novaline.dulin@gmail.com>
2020-03-17 13:11:11 +0800

    add sample

---
+++ src/examples/stackoverflow/60705929/index.ts
@@ -0,0 +1,29 @@
+import { sequelize } from '../../db';
+import Sequelize, { Model } from 'sequelize';
+
+class SomeModel extends Model {}
+SomeModel.init(
+  {
+    data: {
+      type: Sequelize.JSONB,
+    },
+  },
+  { sequelize, modelName: 'somemodels' },
+);
+
+(async function test() {
+  try {
+    await sequelize.sync({ force: true });
+    // insert
+    const data = { name: 'orm', age: 23 };
+    await SomeModel.create({ data });
+
+    // find
+    const row = await SomeModel.findByPk(1, { raw: true });
+    console.log(row);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await sequelize.close();
+  }
+})();
