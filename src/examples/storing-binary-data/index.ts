45609de4164aa7064bcc4562a204959003706b93
mrdulin <novaline.dulin@gmail.com>
2020-03-19 12:19:33 +0800

    add sample

--- src/examples/storing-binary-data/index.ts
+++ src/examples/storing-binary-data/index.ts
@@ -23,11 +23,11 @@ Image.init(
   try {
     await sequelize.sync({ force: true });
     const filepath = path.resolve(__dirname, './image.jpeg');
-    let data = Buffer.from(fs.readFileSync(filepath));
+    const data = Buffer.from(fs.readFileSync(filepath));
     const mimeType = mime.getType(filepath);
     const image = await Image.create({ data, mimeType });
     // console.log(image.data, Buffer.isBuffer(image.data));
-    const result = await sequelize.query('select id, left(encode(data, \'hex\'), 40) from "images";');
+    await sequelize.query('select id, left(encode(data, \'hex\'), 40) from "images";');
     const outputFilepath = path.resolve(__dirname, './output.jpeg');
     fs.writeFileSync(outputFilepath, image.data, 'base64');
   } catch (error) {
