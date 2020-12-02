696cb2d7a76fac13f8ed9cc4e87deda57d4d4fa9
mrdulin <novaline.dulin@gmail.com>
2020-12-02 14:31:28 +0800

    add example

---
+++ src/examples/stackoverflow/57009357/index.ts
@@ -0,0 +1,32 @@
+import { sequelize } from '../../db';
+import { Model, DataTypes } from 'sequelize';
+
+class Communication extends Model {}
+Communication.init(
+  {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    message_uuid: {
+      type: DataTypes.INTEGER,
+      unique: true,
+    },
+  },
+  { sequelize, tableName: 'communications' },
+);
+
+(async function test() {
+  try {
+    await sequelize.sync({ force: true });
+    // seed
+    await Communication.create({ firstname: 'teresa', lastname: 'teng', age: 32, message_uuid: 123 });
+    // test
+    await Communication.bulkCreate([{ firstname: 'teresa', lastname: 'teng', age: 32, message_uuid: 123 }], {
+      ignoreDuplicates: true,
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await sequelize.close();
+  }
+})();
