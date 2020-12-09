13bed7f680227f74b9ee076caacb24649c97d2b2
mrdulin <novaline.dulin@gmail.com>
2020-12-09 12:56:52 +0800

    add example

---
+++ src/examples/stackoverflow/65209821/index.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db';
+
+export class Exams extends Model {}
+
+export class ExamsModel {
+  id!: number;
+  name!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+  deleted!: boolean;
+}
+
+Exams.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field: 'idExam',
+    },
+    name: {
+      type: DataTypes.STRING,
+      field: 'name',
+    },
+    deletedAt: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Exams',
+    modelName: 'Exams',
+    schema: 'operation',
+    underscored: false,
+    freezeTableName: true, // not pluralizes
+  },
+);
+
+(async function test() {
+  try {
+    await sequelize.sync({ force: true });
+    const data = await Exams.findAll();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await sequelize.close();
+  }
+})();
