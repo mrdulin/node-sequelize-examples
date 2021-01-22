76ff2cb4e7ac50b92526e8fc34d6bcac8ba05151
mrdulin <novaline.dulin@gmail.com>
2021-01-22 17:18:36 +0800

    update example

--- src/examples/stackoverflow/65834694/main.ts
+++ src/examples/stackoverflow/65834694/main.ts
@@ -7,6 +7,8 @@ class User extends Model {
   public getPreferences!: HasManyGetAssociationsMixin<Preference>;
   public setPreferences!: HasManySetAssociationsMixin<Preference, Preference['id']>;
   public readonly Preferences?: Preference[];
+
+  public _inMemoryAssociatedPreferences: Preference[] = [];
 }
 User.init({}, { sequelize });
 
@@ -14,32 +16,40 @@ class Preference extends Model {
   public id!: number;
   public name!: string;
 }
-Preference.init(
-  {
-    name: DataTypes.STRING,
-  },
-  { sequelize },
-);
+Preference.init({ name: DataTypes.STRING }, { sequelize });
 
 User.hasMany(Preference);
 Preference.belongsTo(User);
 
 (async function test() {
-  // https://sequelize.org/master/manual/typescript.html
-  console.log(User.associations.preferences);
   await sequelize.sync({ force: true });
-
   await User.create({ id: 1 });
-
   const user: User = await User.findOne({ where: { id: 1 } });
-  console.log(user.Preferences);
-
   const pref = await Preference.create({ id: 1, name: 'a' });
   await user.setPreferences(pref);
-  console.log(user.Preferences);
+  user._inMemoryAssociatedPreferences = [pref];
 
-  const user2: User = await User.findOne({ where: { id: 1 }, include: [Preference] });
-  console.log(user2.Preferences); // it's only populated when explicitly requested in code
-
+  // elsewhere
+  console.log('in memory preferences:', user._inMemoryAssociatedPreferences);
   await sequelize.close();
 })();
+
+// (async function test() {
+//   // https://sequelize.org/master/manual/typescript.html
+//   console.log(User.associations.preferences);
+//   await sequelize.sync({ force: true });
+
+//   await User.create({ id: 1 });
+
+//   const user: User = await User.findOne({ where: { id: 1 } });
+//   console.log(user.Preferences);
+
+//   const pref = await Preference.create({ id: 1, name: 'a' });
+//   await user.setPreferences(pref);
+//   console.log(user.Preferences);
+
+//   const user2: User = await User.findOne({ where: { id: 1 }, include: [Preference] });
+//   console.log(user2.Preferences); // it's only populated when explicitly requested in code
+
+//   await sequelize.close();
+// })();
